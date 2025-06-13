292d24fba51dcf9e1e2b2dc22c9e90996d0c694f
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-13 08:52:00 +0200

    v4.0

--- 1.6/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/IdeoUIUtility_AddPrecept.cs
+++ 1.6/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/IdeoUIUtility_AddPrecept.cs
@@ -9,22 +9,22 @@ using System.Reflection.Emit;
 namespace VanillaMemesExpanded
 {
     [HarmonyPatch(typeof(IdeoUIUtility))]
-    [HarmonyPatch("DoPreceptsInt")]
-    public static class VanillaMemesExpanded_IdeoUIUtility_DoPreceptsInt_Patch
+    [HarmonyPatch("AddPrecept")]
+    public static class VanillaMemesExpanded_IdeoUIUtility_AddPrecept_Patch
     {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var codes = instructions.ToList();
 
             for (var i = 0; i < codes.Count; i++)
             {
-
                 var code = codes[i];
-                if (i > 0 && codes[i - 1].opcode == OpCodes.Ldloc_S && codes[i - 1].operand is LocalBuilder lb && lb.LocalIndex == 17)
+                if (i > 0 && codes[i - 1].opcode == OpCodes.Ldloc_2 && codes[i].opcode == OpCodes.Ldc_I4_6)
                 {
                     yield return new CodeInstruction(OpCodes.Ldc_I4, (int)VanillaMemesExpanded_Settings.ritualsAmount);
                 }
-                else if (i > 0 && codes[i - 1].opcode == OpCodes.Ldstr&&(string)codes[i - 1].operand== "MaxRitualCount")
+                else if (i > 0 && codes[i - 1].opcode == OpCodes.Ldstr && (string)codes[i - 1].operand == "MaxRitualCount")
                 {
                     yield return new CodeInstruction(OpCodes.Ldc_I4, (int)VanillaMemesExpanded_Settings.ritualsAmount);
                 }
