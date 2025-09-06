600a8b950dcc36c6680894bec362aa9c9382e650
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-06 15:04:13 +0200

    v4.04

---
+++ 1.6/Source/VanillaMemesExpanded/VanillaMemesExpanded/Harmony/Thing_PreSwapMap.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Verse.AI;
+
+namespace VanillaMemesExpanded
+{
+
+    [HarmonyPatch(typeof(Thing))]
+    [HarmonyPatch("PreSwapMap")]
+    public static class VanillaMemesExpanded_Thing_PreSwapMap_Patch
+    {
+        [HarmonyPostfix]
+        static void SetPawnCaravanTimerToZero(Thing __instance)
+        {
+            if (__instance is Pawn pawn && pawn.Spawned && pawn.IsFreeColonist)
+            {
+                WorldComponent_TravellingAndTradingTracker.Instance.AddColonistToCaravanList(pawn, 0);
+                WorldComponent_TravellingAndTradingTracker.Instance.ResetPawnCaravanTicks(pawn);
+            }
+
+        }
+    }
+}
